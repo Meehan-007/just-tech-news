444313751a84e3335e80647ee223f98013c93699
Patrick Meehan <meeha@MacBook-Air.local>
2022-06-07 22:25:23 -0400

    done

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'Sallies12!', { 
+    
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
