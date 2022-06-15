d8d89926ab53dff83dde190ac4a533dc59a10989
Patrick Meehan <meeha@MacBook-Air.local>
2022-06-15 19:35:10 -0400

    done

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'Sallies12!', { 
-    
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
 
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize('just_tech_news_db', 'root', 'Root122!', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 module.exports = sequelize;
